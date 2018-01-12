d72cabd31cdbeeeb545e6e4e2b37cd501124dc86
christine <christine@christine.nl>
2018-01-12 17:35:12 +0100

    changed permissions code

--- app/src/main/java/nl/christine/demo/activity/MainActivity.java
+++ app/src/main/java/nl/christine/demo/activity/MainActivity.java
@@ -22,6 +22,7 @@ import android.widget.ListView;
 
 import org.apache.commons.io.IOUtils;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
@@ -36,44 +37,40 @@ import nl.christine.demo.list.CsvListAdapter;
 public class MainActivity extends BaseActivity {
 
     private ListView listView;
-    private String firstRun = "first_run";
 
     private static final int MY_PERMISSIONS_REQUEST_WRITE_STORAGE = 1001;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    doIt();
 
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-
-                runOnUiThread(new Runnable() {
-
-                    @Override
-                    public void run() {
-                        showAlert(getResources().getString(R.string.storage_permission_text));
-                    }
-                });
-
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                        MY_PERMISSIONS_REQUEST_WRITE_STORAGE);
+                } else {
+                    showAlert(getString(R.string.no_permissions));
+                }
+                return;
             }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
         }
 
-        if (PreferenceManager.getDefaultSharedPreferences(this).getBoolean(firstRun, true)) {
-            String issuesFileName = "/sdcard/" + getString(R.string.issues_file);
+        doIt();
+    }
+
+    private void doIt() {
+        String issuesFileName = "/sdcard/" + getString(R.string.issues_file);
+        File file = new File(issuesFileName);
+
+        if (!file.exists()) {
             try {
                 IOUtils.copy(getAssets().open(getString(R.string.issues_file)), new FileOutputStream(issuesFileName));
-                SharedPreferences.Editor edit = PreferenceManager.getDefaultSharedPreferences(this).edit();
-                edit.putBoolean(firstRun, false);
-                edit.commit();
             } catch (IOException e) {
                 showAlert(e);
             }
@@ -102,7 +99,21 @@ public class MainActivity extends BaseActivity {
 
             }
         });
+    }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_WRITE_STORAGE);
+        } else {
+            doIt();
+        }
     }
 }
